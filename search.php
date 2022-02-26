af319d958ea99ef579c89f7a90376d7f99674b8c
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-26 12:07:53 +0100

    Update search.php

--- search.php
+++ search.php
@@ -2,7 +2,7 @@
 // cerchiamo insieme la robba buona
 $query = str_replace("%20", " ", $_GET["query"]);
 
-if (empty($q)) {
+if (empty($query)) {
   echo "<h1>Index of Patreon/</h1><br><hr><br>";
   require_once("searchBar.php");
   foreach (glob("Patreon/*") as $file) {
