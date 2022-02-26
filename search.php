05478a2e86f79788df60c969febc6a9ebc90699b
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-26 12:03:34 +0100

    Update search.php

--- search.php
+++ search.php
@@ -1,15 +1,17 @@
 <?php
 // cerchiamo insieme la robba buona
-$query = $_POST["q"];
+$query = str_replace("%20", " ", $_GET["q"]);
 
 if (empty($q)) {
   echo "<h1>Index of Patreon/</h1><br><hr><br>";
+  require_once("searchBar.php");
   foreach (glob("Patreon/*") as $file) {
     $filename = str_replace("Patreon/", "", $file);
     echo "<a href='Patreon/$filename/'>$filename</a> &nbsp;&nbsp;&nbsp;&nbsp;<a href='wiewSequence?load=$filename'>[S]</a><br>";
   }
 } else {
   echo "<h1>Index of Patreon/ filtered by $query</h1><br><hr><br>";
+  require_once("searchBar.php");
   foreach (glob("Patreon/*") as $file) {
     $filename = str_replace("Patreon/", "", $file);
     if (stripos($filename, $query) !== false) {
